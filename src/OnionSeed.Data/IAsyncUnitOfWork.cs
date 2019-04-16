cfe622341cc4470f1261782422bcc17822e19559
RJ Hollberg <rj.hollberg@outlook.com>
2019-04-16 16:03:22 -0600

    Add IUnitOfWork and IAsyncUnitOfWork definitions.

---
+++ src/OnionSeed.Data/IAsyncUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace OnionSeed.Data
+{
+	/// <summary>
+	/// Defines an atomic transaction whose changes are to be asynchronously committed as a collective whole.
+	/// </summary>
+	public interface IAsyncUnitOfWork
+	{
+		/// <summary>
+		/// Commits any pending changes.
+		/// </summary>
+		/// <returns>A task representing the operation.</returns>
+		Task CommitAsync();
+	}
+}
---
+++ src/OnionSeed.Data/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace OnionSeed.Data
+{
+	/// <summary>
+	/// Defines an atomic transaction whose changes are to be committed as a collective whole.
+	/// </summary>
+	public interface IUnitOfWork
+	{
+		/// <summary>
+		/// Commits any pending changes.
+		/// </summary>
+		void Commit();
+	}
+}
