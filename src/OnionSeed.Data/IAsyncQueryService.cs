18be9363e6f3351742e415e93b1f7e91c035b672
RJ Hollberg <rj.hollberg@outlook.com>
2019-04-16 16:00:49 -0600

    Add IAsyncQueryService and IAsyncRepository definitions.

---
+++ src/OnionSeed.Data/IAsyncQueryService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OnionSeed.Data
+{
+	/// <summary>
+	/// Defines a mechanism that can be used to asynchronously query entities from a data store.
+	/// </summary>
+	/// <typeparam name="TEntity">The type of entities in the data store.</typeparam>
+	/// <typeparam name="TIdentity">The type of the unique identity value of the entities in the data store.</typeparam>
+	public interface IAsyncQueryService<TEntity, in TIdentity>
+		where TEntity : IEntity<TIdentity>
+		where TIdentity : IEquatable<TIdentity>, IComparable<TIdentity>
+	{
+		/// <summary>
+		/// Gets the number of entities in the data store.
+		/// </summary>
+		/// <returns>A task representing the operation. Upon completion, it will contain
+		/// the number of entities in the data store.</returns>
+		Task<long> GetCountAsync();
+
+		/// <summary>
+		/// Begins an enumeration of all entities in the data store.
+		/// </summary>
+		/// <returns>A task representing the operation. Upon completion, it will contain
+		/// an enumeration of all entities in the data store.</returns>
+		Task<IEnumerable<TEntity>> GetAllAsync();
+
+		/// <summary>
+		/// Gets a specific entity from the data store by its unique identity value.
+		/// </summary>
+		/// <param name="id">The unique identity value of the entity.</param>
+		/// <returns>A task representing the operation. Upon completion, it will contain
+		/// the entity that has the given unique identity value.</returns>
+		/// <exception cref="ArgumentNullException"><paramref name="id"/> is <c>null</c>.</exception>
+		/// <exception cref="KeyNotFoundException">The specified unique identity value does not exist in the data store.</exception>
+		Task<TEntity> GetByIdAsync(TIdentity id);
+
+		/// <summary>
+		/// Attempts to get a specific entity from the data store by its unique identity value.
+		/// </summary>
+		/// <param name="id">The unique identity value of the entity.</param>
+		/// <returns>A task representing the operation. Upon completion, it will contain
+		/// the entity that has the given unique identity value,
+		/// or <c>null</c> if the specified entity was not found.</returns>
+		/// <exception cref="ArgumentNullException"><paramref name="id"/> is <c>null</c>.</exception>
+		Task<TEntity> TryGetByIdAsync(TIdentity id);
+	}
+}
