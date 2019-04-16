c1501b688c1c20e1a4f65a320489450399d40506
RJ Hollberg <rj.hollberg@outlook.com>
2019-04-16 15:55:53 -0600

    Add IQueryService and IRepository definitions.

---
+++ src/OnionSeed.Data/IQueryService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnionSeed.Data
+{
+	/// <summary>
+	/// Defines a mechanism that can be used to query entities from a data store.
+	/// </summary>
+	/// <typeparam name="TEntity">The type of entities in the data store.</typeparam>
+	/// <typeparam name="TIdentity">The type of the unique identity value of the entities in the data store.</typeparam>
+	public interface IQueryService<TEntity, in TIdentity>
+		where TEntity : IEntity<TIdentity>
+		where TIdentity : IEquatable<TIdentity>, IComparable<TIdentity>
+	{
+		/// <summary>
+		/// Gets the number of entities in the data store.
+		/// </summary>
+		/// <returns>The number of entities in the data store.</returns>
+		long GetCount();
+
+		/// <summary>
+		/// Begins an enumeration of all entities in the data store.
+		/// </summary>
+		/// <returns>An enumeration of all entities in the data store.</returns>
+		IEnumerable<TEntity> GetAll();
+
+		/// <summary>
+		/// Gets a specific entity from the data store by its unique identity value.
+		/// </summary>
+		/// <param name="id">The unique identity value of the entity.</param>
+		/// <returns>The entity that has the given unique identity value.</returns>
+		/// <exception cref="ArgumentNullException"><paramref name="id"/> is <c>null</c>.</exception>
+		/// <exception cref="KeyNotFoundException">The specified unique identity value does not exist in the data store.</exception>
+		TEntity GetById(TIdentity id);
+
+		/// <summary>
+		/// Attempts to get a specific entity from the data store by its unique identity value.
+		/// </summary>
+		/// <param name="id">The unique identity value of the entity.</param>
+		/// <param name="result">When this method returns, contains the entity that has the given unique identity value,
+		/// or <c>null</c> if the specified entity was not found. This parameter is passed uninitialized;
+		/// any value originally supplied in <paramref name="result"/> will be overwritten.</param>
+		/// <returns><c>true</c> if the specified entity was found; otherwise, <c>false</c>.</returns>
+		/// <exception cref="ArgumentNullException"><paramref name="id"/> is <c>null</c>.</exception>
+		bool TryGetById(TIdentity id, out TEntity result);
+	}
+}
