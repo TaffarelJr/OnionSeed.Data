db662040f9be86fedfa0ef4a9697ea0a1fe112d3
RJ Hollberg <rj.hollberg@outlook.com>
2019-05-03 13:20:17 -0600

    Add SequentialGuidFactory and tests.

---
+++ src/OnionSeed.Data/Factories/SequentialGuidType.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace OnionSeed.Data.Factories
+{
+	/// <summary>
+	/// Identifies the different ways a sequential GUID can be structured.
+	/// </summary>
+	/// <remarks>Different databases store GUID values in different ways internally.
+	/// If this is not taken into consideration, GUID values can be much slower to persist than simple integer values.
+	/// But by selecting a format that matches a database's storage particulars, we can largely negate that overhead.</remarks>
+	public enum SequentialGuidType
+	{
+		/// <summary>
+		/// Indicates that the GUID values will be stored as strings in the database,
+		/// with the sequential data at the beginning of the string value of the GUID.
+		/// <p>This works well for:
+		/// <list type="bullet">
+		///   <item>
+		///     <description>MySQL (using <c>char(36)</c>)</description>
+		///   </item>
+		///   <item>
+		///     <description>PostgreSQL (using <c>uuid</c>)</description>
+		///   </item>
+		///   <item>
+		///     <description>SQLite (when stored as 36 characters of text)</description>
+		///   </item>
+		/// </list></p></summary>
+		[SuppressMessage("Naming", "CA1720:Identifier contains type name", Justification = "Approved as part of this enum.")]
+		String = 0,
+
+		/// <summary>
+		/// Indicates that the GUID values will be stored as raw binary data.
+		/// <p>This works well for:
+		/// <list type="bullet">
+		///   <item>
+		///     <description>Oracle (using <c>raw(16)</c>)</description>
+		///   </item>
+		///   <item>
+		///     <description>SQLite (when stored as 16 bytes of binary data)</description>
+		///   </item>
+		/// </list></p></summary>
+		Binary = 1,
+
+		/// <summary>
+		/// Indicates that the GUID values will be stored as raw binary data,
+		/// with the least-significant bytes containing the sequential data.
+		/// <p>This works well for:
+		/// <list type="bullet">
+		///   <item>
+		///     <description>SQL Server (using <c>uniqueidentifier</c>)</description>
+		///   </item>
+		/// </list></p></summary>
+		BinaryEnd = 2
+	}
+}
---
+++ test/OnionSeed.Data.Tests/Factories/SequentialGuidFactoryTests.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace OnionSeed.Data.Factories
+{
+	public class SequentialGuidFactoryTests
+	{
+		private const int TestIterations = 100000;
+
+		[Fact]
+		public void Constructor_ShouldThrowException_WhenTypeIsNotRecognized()
+		{
+			// Act
+			Action action = () => new SequentialGuidFactory((SequentialGuidType)10);
+
+			// Assert
+			action.Should().Throw<ArgumentOutOfRangeException>();
+		}
+
+		[Fact]
+		public void CreateNew_ShouldReturnNewGuid_WithoutCollisions_WhenTypeIsBinary()
+		{
+			// Arrange
+			var uniqueResults = new HashSet<Guid>();
+			var sequentialData = new string[TestIterations];
+			var subject = new SequentialGuidFactory(SequentialGuidType.Binary);
+
+			for (var i = 0; i < TestIterations; i++)
+			{
+				// Act
+				var result = subject.CreateNew();
+
+				// Assert
+				uniqueResults.Add(result).Should().BeTrue();
+				var sequentialBytes = result.ToByteArray().Take(SequentialGuidFactory.NumberOfSequentialBytes).ToArray();
+				sequentialData[i] = BitConverter.ToString(sequentialBytes);
+			}
+
+			uniqueResults.Should().HaveCount(TestIterations);
+			uniqueResults.Should().NotContain(Guid.Empty);
+
+			sequentialData.Should().BeInAscendingOrder();
+		}
+
+		[Fact]
+		public void CreateNew_Benchmark_WhenTypeIsBinary()
+		{
+			// Arrange
+			var subject = new SequentialGuidFactory(SequentialGuidType.Binary);
+
+			for (var i = 0; i < TestIterations; i++)
+			{
+				// Act
+				subject.CreateNew();
+			}
+		}
+
+		[Fact]
+		public void CreateNew_ShouldReturnNewGuid_WithoutCollisions_WhenTypeIsString()
+		{
+			// Arrange
+			const int numberOfChars = (SequentialGuidFactory.NumberOfSequentialBytes * 2) + 1;  // 2 chars per byte, plus one hyphen
+
+			var uniqueResults = new HashSet<Guid>();
+			var sequentialData = new string[TestIterations];
+			var subject = new SequentialGuidFactory(SequentialGuidType.String);
+
+			for (var i = 0; i < TestIterations; i++)
+			{
+				// Act
+				var result = subject.CreateNew();
+
+				// Assert
+				uniqueResults.Add(result).Should().BeTrue();
+				sequentialData[i] = result.ToString().Substring(0, numberOfChars);
+			}
+
+			uniqueResults.Should().HaveCount(TestIterations);
+			uniqueResults.Should().NotContain(Guid.Empty);
+
+			sequentialData.Should().BeInAscendingOrder();
+		}
+
+		[Fact]
+		public void CreateNew_Benchmark_WhenTypeIsString()
+		{
+			// Arrange
+			var subject = new SequentialGuidFactory(SequentialGuidType.String);
+
+			for (var i = 0; i < TestIterations; i++)
+			{
+				// Act
+				subject.CreateNew();
+			}
+		}
+
+		[Fact]
+		public void CreateNew_ShouldReturnNewGuid_WithoutCollisions_WhenTypeIsBinaryEnd()
+		{
+			// Arrange
+			const int numberOfChars = SequentialGuidFactory.NumberOfSequentialBytes * 2;  // 2 chars per byte
+
+			var uniqueResults = new HashSet<Guid>();
+			var sequentialData = new string[TestIterations];
+			var subject = new SequentialGuidFactory(SequentialGuidType.BinaryEnd);
+
+			for (var i = 0; i < TestIterations; i++)
+			{
+				// Act
+				var result = subject.CreateNew();
+
+				// Assert
+				uniqueResults.Add(result).Should().BeTrue();
+				var guidString = result.ToString();
+				sequentialData[i] = guidString.Substring(guidString.Length - numberOfChars, numberOfChars);
+			}
+
+			uniqueResults.Should().HaveCount(TestIterations);
+			uniqueResults.Should().NotContain(Guid.Empty);
+
+			sequentialData.Should().BeInAscendingOrder();
+		}
+
+		[Fact]
+		public void CreateNew_Benchmark_WhenTypeIsBinaryEnd()
+		{
+			// Arrange
+			var subject = new SequentialGuidFactory(SequentialGuidType.BinaryEnd);
+
+			for (var i = 0; i < TestIterations; i++)
+			{
+				// Act
+				subject.CreateNew();
+			}
+		}
+	}
+}
