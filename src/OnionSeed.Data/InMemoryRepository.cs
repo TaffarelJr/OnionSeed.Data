a24bb05808847a07ae9a083f98643fb194d298a4
RJ Hollberg <rj.hollberg@outlook.com>
2019-05-05 08:20:31 -0600

    Add In-Memory repository implementation.

---
+++ src/OnionSeed.Data/InMemoryRepository.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using OnionSeed.Types;
+
+namespace OnionSeed.Data
+{
+	/// <inheritdoc />
+	/// <summary>
+	/// A basic implementation of <see cref="IRepository{T,TId}"/> that simply stores data in an in-memory dictionary.
+	/// </summary>
+	/// <remarks>Since this repository is fast and contains no calls to external services,
+	/// the functionality is implemented in a synchronous fashion.
+	/// <p>This class is thread-safe.</p></remarks>
+	[SuppressMessage("AsyncUsage.CSharp.Naming", "AvoidAsyncSuffix:Avoid Async suffix", Justification = "ValueTask is returned instead of Task.")]
+	public class InMemoryRepository<T, TId> : IRepository<T, TId>
+		where T : IEntity<TId>
+		where TId : IEquatable<TId>, IComparable<TId>
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="InMemoryRepository{T,TId}"/> class.
+		/// </summary>
+		public InMemoryRepository()
+			: this(null)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="InMemoryRepository{T,TId}"/> class
+		/// using the given list of entities as its initial data set.
+		/// </summary>
+		/// <param name="entities">The list of entities to be used as the initial data set.</param>
+		/// <remarks>The list of entities is copied, so no references to it are kept internally.</remarks>
+		public InMemoryRepository(IEnumerable<T> entities)
+		{
+			RawData = new ConcurrentDictionary<TId, T>(
+				(entities ?? Enumerable.Empty<T>())
+					.Select(e => new KeyValuePair<TId, T>(e.Id, e)));
+		}
+
+		/// <summary>
+		/// Gets a reference to the raw data store.
+		/// </summary>
+		public ConcurrentDictionary<TId, T> RawData { get; }
+
+#if COMPLETED_TASK
+		private static Task CompletedTask => Task.CompletedTask;
+#else
+		private static Task CompletedTask => Task.FromResult(true);
+#endif
+
+		/// <inheritdoc />
+		public ValueTask<int> GetCountAsync() => new ValueTask<int>(RawData.Count);
+
+		/// <inheritdoc />
+		public ValueTask<IEnumerable<T>> GetAllAsync() => new ValueTask<IEnumerable<T>>(RawData.Values);
+
+		/// <inheritdoc />
+		public ValueTask<T> GetByIdAsync(TId id)
+		{
+			ValidateId(id);
+
+			if (RawData.TryGetValue(id, out var result))
+				return new ValueTask<T>(result);
+
+			throw new KeyNotFoundException($"Entity '{id}' was not found.");
+		}
+
+		/// <inheritdoc />
+		public ValueTask<T> TryGetByIdAsync(TId id)
+		{
+			ValidateId(id);
+
+			RawData.TryGetValue(id, out var result);
+			return new ValueTask<T>(result);
+		}
+
+		/// <inheritdoc />
+		public Task AddAsync(T item)
+		{
+			ValidateItemAndId(item);
+
+			if (!RawData.TryAdd(item.Id, item))
+				throw new ArgumentException($"Entity '{item.Id}' already exists.");
+
+			return CompletedTask;
+		}
+
+		/// <inheritdoc />
+		public Task AddOrUpdateAsync(T item)
+		{
+			ValidateItemAndId(item);
+			RawData.AddOrUpdate(item.Id, item, (k, v) => item);
+			return Task.FromResult(true);
+		}
+
+		/// <inheritdoc />
+		public Task UpdateAsync(T item)
+		{
+			ValidateItemAndId(item);
+
+			var success = false;
+			while (!success)
+			{
+				if (!RawData.TryGetValue(item.Id, out var previous))
+					throw new KeyNotFoundException($"Entity '{item.Id}' was not found.");
+
+				success = RawData.TryUpdate(item.Id, item, previous);
+			}
+
+			return CompletedTask;
+		}
+
+		/// <inheritdoc />
+		public Task RemoveAsync(T item)
+		{
+			ValidateItemAndId(item);
+			RawData.TryRemove(item.Id, out var _);
+			return CompletedTask;
+		}
+
+		/// <inheritdoc />
+		public Task RemoveAsync(TId id)
+		{
+			ValidateId(id);
+			RawData.TryRemove(id, out var _);
+			return CompletedTask;
+		}
+
+		/// <inheritdoc />
+		public ValueTask<bool> TryAddAsync(T item)
+		{
+			ValidateItemAndId(item);
+			return new ValueTask<bool>(RawData.TryAdd(item.Id, item));
+		}
+
+		/// <inheritdoc />
+		public ValueTask<bool> TryUpdateAsync(T item)
+		{
+			ValidateItemAndId(item);
+
+			var success = false;
+			while (!success)
+			{
+				if (!RawData.TryGetValue(item.Id, out var previous))
+					return new ValueTask<bool>(false);
+
+				success = RawData.TryUpdate(item.Id, item, previous);
+			}
+
+			return new ValueTask<bool>(true);
+		}
+
+		/// <inheritdoc />
+		public ValueTask<bool> TryRemoveAsync(T item)
+		{
+			ValidateItemAndId(item);
+			return new ValueTask<bool>(RawData.TryRemove(item.Id, out var _));
+		}
+
+		/// <inheritdoc />
+		public ValueTask<bool> TryRemoveAsync(TId id)
+		{
+			ValidateId(id);
+			return new ValueTask<bool>(RawData.TryRemove(id, out var _));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static void ValidateId(TId id)
+		{
+			if (id == null)
+				throw new ArgumentNullException(nameof(id));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static void ValidateItemAndId(T item)
+		{
+			if (item == null)
+				throw new ArgumentNullException(nameof(item));
+			if (item.Id == null)
+				throw new ArgumentNullException(nameof(item), "Entity has a null ID.");
+		}
+	}
+}
---
+++ test/OnionSeed.Data.Tests/InMemoryRepositoryTests.cs
@@ -0,0 +1,675 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+#pragma warning disable IDE0039 // Use local function
+#pragma warning disable UseAsyncSuffix
+
+namespace OnionSeed.Data
+{
+	public class InMemoryRepositoryTests
+	{
+		private readonly List<FakeEntity<int>> _entities = new List<FakeEntity<int>>
+		{
+			new FakeEntity<int>(1, "Bill"),
+			new FakeEntity<int>(2, "Jane"),
+			new FakeEntity<int>(3, "Jake"),
+			new FakeEntity<int>(4, "Megan")
+		};
+
+		[Fact]
+		public void Constructor_ShouldCreateEmptyRepository_WhenNoDataIsGiven()
+		{
+			// Act
+			var result = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Assert
+			result.RawData.Count.Should().Be(0);
+		}
+
+		[Fact]
+		public void Constructor_ShouldCreateEmptyRepository_WhenInitialDataIsNull()
+		{
+			// Act
+			var result = new InMemoryRepository<FakeEntity<int>, int>(null);
+
+			// Assert
+			result.RawData.Count.Should().Be(0);
+		}
+
+		[Fact]
+		[SuppressMessage("ReSharper", "ObjectCreationAsStatement", Justification = "Testing constructor.")]
+		public void Constructor_ShouldThrowException_WhenInitialDataContainsDuplicates()
+		{
+			// Act
+			Action action = () => new InMemoryRepository<FakeEntity<int>, int>(_entities.Concat(_entities));
+
+			// Assert
+			action.ShouldThrow<ArgumentException>();
+		}
+
+		[Fact]
+		public void Constructor_ShouldLoadRepository_WhenInitialDataIsGiven()
+		{
+			// Act
+			var result = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Assert
+			result.RawData.Values.Should().BeEquivalentTo(_entities);
+		}
+
+		[Fact]
+		public async Task GetCountAsync_ShouldReturnNumberOfItems()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			var result = await subject.GetCountAsync().ConfigureAwait(false);
+
+			// Assert
+			result.Should().Be(_entities.Count);
+		}
+
+		[Fact]
+		public async Task GetAllAsync_ShouldReturnEmptyList_WhenRepositoryIsEmpty()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			var result = (await subject.GetAllAsync().ConfigureAwait(false)).ToList();
+
+			// Assert
+			result.Count.Should().Be(0);
+		}
+
+		[Fact]
+		public async Task GetAllAsync_ShouldReturnAllEntities_WhenRepositoryHasData()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			var result = (await subject.GetAllAsync().ConfigureAwait(false)).ToList();
+
+			// Assert
+			result.Should().BeEquivalentTo(_entities);
+		}
+
+		[Fact]
+		public void GetByIdAsync_ShouldThrowException_WhenIdIsNull()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<string>, string>();
+
+			// Act
+			Func<Task> action = async () => await subject.GetByIdAsync(null).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public void GetByIdAsync_ShouldThrowException_WhenWhenEntityDoesNotExist()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			Func<Task> action = async () => await subject.GetByIdAsync(27).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<KeyNotFoundException>();
+		}
+
+		[Fact]
+		public async Task GetByIdAsync_ShouldReturnEntity_WhenEntityExists()
+		{
+			// Arrange
+			var person = _entities[1];
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			var result = await subject.GetByIdAsync(person.Id).ConfigureAwait(false);
+
+			// Assert
+			result.Should().BeSameAs(person);
+		}
+
+		[Fact]
+		public void TryGetByIdAsync_ShouldThrowException_WhenIdIsNull()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<string>, string>();
+
+			// Act
+			Func<Task> action = async () => await subject.TryGetByIdAsync(null).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public async Task TryGetByIdAsync_ShouldReturnNull_WhenWhenEntityDoesNotExist()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			var result = await subject.TryGetByIdAsync(27).ConfigureAwait(false);
+
+			// Assert
+			result.Should().BeNull();
+		}
+
+		[Fact]
+		public async Task TryGetByIdAsync_ShouldReturnEntity_WhenEntityExists()
+		{
+			// Arrange
+			var person = _entities[1];
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			var result = await subject.TryGetByIdAsync(person.Id).ConfigureAwait(false);
+
+			// Assert
+			result.Should().BeSameAs(person);
+		}
+
+		[Fact]
+		public void AddAsync_ShouldThrowException_WhenEntityIsNull()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			Func<Task> action = async () => await subject.AddAsync(null).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public void AddAsync_ShouldThrowException_WhenIdIsNull()
+		{
+			// Arrange
+			var person = new FakeEntity<string>(null, "Brandon");
+			var subject = new InMemoryRepository<FakeEntity<string>, string>();
+
+			// Act
+			Func<Task> action = async () => await subject.AddAsync(person).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public async Task AddAsync_ShouldAddEntityToRepository_WhenEntityDoesNotExist()
+		{
+			// Arrange
+			var person = new FakeEntity<int>(10, "Brandon");
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			await subject.AddAsync(person).ConfigureAwait(false);
+
+			// Assert
+			subject.RawData.Count.Should().Be(1);
+			subject.RawData.Should().ContainKey(person.Id);
+			subject.RawData[person.Id].Should().BeSameAs(person);
+		}
+
+		[Fact]
+		public void AddAsync_ShouldThrowException_WhenEntityExists()
+		{
+			// Arrange
+			var person = _entities[1];
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			Func<Task> action = async () => await subject.AddAsync(person).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentException>();
+		}
+
+		[Fact]
+		public void AddOrUpdateAsync_ShouldThrowException_WhenEntityIsNull()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			Func<Task> action = async () => await subject.AddOrUpdateAsync(null).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public void AddOrUpdateAsync_ShouldThrowException_WhenIdIsNull()
+		{
+			// Arrange
+			var person = new FakeEntity<string>(null, "Brandon");
+			var subject = new InMemoryRepository<FakeEntity<string>, string>();
+
+			// Act
+			Func<Task> action = async () => await subject.AddOrUpdateAsync(person).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public async Task AddOrUpdateAsync_ShouldAddEntityToRepository_WhenEntityDoesNotExist()
+		{
+			// Arrange
+			var person = new FakeEntity<int>(10, "Brandon");
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			await subject.AddOrUpdateAsync(person).ConfigureAwait(false);
+
+			// Assert
+			subject.RawData.Count.Should().Be(1);
+			subject.RawData.Should().ContainKey(person.Id);
+			subject.RawData[person.Id].Should().BeSameAs(person);
+		}
+
+		[Fact]
+		public async Task AddOrUpdateAsync_ShouldUpdateEntityInRepository_WhenEntityExists()
+		{
+			// Arrange
+			var person = new FakeEntity<int>(1, "Brandon");
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			await subject.AddOrUpdateAsync(person).ConfigureAwait(false);
+
+			// Assert
+			subject.RawData.Count.Should().Be(_entities.Count);
+			subject.RawData.Should().ContainKey(person.Id);
+			subject.RawData[person.Id].Should().BeSameAs(person);
+		}
+
+		[Fact]
+		public void UpdateAsync_ShouldThrowException_WhenEntityIsNull()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			Func<Task> action = async () => await subject.UpdateAsync(null).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public void UpdateAsync_ShouldThrowException_WhenIdIsNull()
+		{
+			// Arrange
+			var person = new FakeEntity<string>(null, "Brandon");
+			var subject = new InMemoryRepository<FakeEntity<string>, string>();
+
+			// Act
+			Func<Task> action = async () => await subject.UpdateAsync(person).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public void UpdateAsync_ShouldThrowException_WhenEntityDoesNotExist()
+		{
+			// Arrange
+			var person = _entities[1];
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			Func<Task> action = async () => await subject.UpdateAsync(person).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<KeyNotFoundException>();
+		}
+
+		[Fact]
+		public async Task UpdateAsync_ShouldUpdateEntityInRepository_WhenEntityExists()
+		{
+			// Arrange
+			var person = new FakeEntity<int>(1, "Brandon");
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			await subject.UpdateAsync(person).ConfigureAwait(false);
+
+			// Assert
+			subject.RawData.Count.Should().Be(_entities.Count);
+			subject.RawData.Should().ContainKey(person.Id);
+			subject.RawData[person.Id].Should().BeSameAs(person);
+		}
+
+		[Fact]
+		public void RemoveAsync_ShouldThrowException_WhenEntityIsNull()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			Func<Task> action = async () => await subject.RemoveAsync(null).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public void RemoveAsync_ShouldThrowException_WhenIdIsNull()
+		{
+			// Arrange
+			var person = new FakeEntity<string>(null, "Brandon");
+			var subject = new InMemoryRepository<FakeEntity<string>, string>();
+
+			// Act
+			Func<Task> action = async () => await subject.RemoveAsync(person).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public async Task RemoveAsync_ShouldDoNothing_WhenEntityDoesNotExist()
+		{
+			// Arrange
+			var person = _entities[1];
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			await subject.RemoveAsync(person).ConfigureAwait(false);
+
+			// Assert
+			subject.RawData.Count.Should().Be(0);
+		}
+
+		[Fact]
+		public async Task RemoveAsync_ShouldRemoveEntityFromRepository_WhenEntityExists()
+		{
+			// Arrange
+			var person = _entities[1];
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			await subject.RemoveAsync(person).ConfigureAwait(false);
+
+			// Assert
+			subject.RawData.Count.Should().Be(_entities.Count - 1);
+			subject.RawData.Should().NotContainKey(person.Id);
+		}
+
+		[Fact]
+		public void RemoveAsync_ShouldThrowException_WhenIdIsGiven_AndIdIsNull()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<string>, string>();
+
+			// Act
+			Func<Task> action = async () => await subject.RemoveAsync((string)null).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public async Task RemoveAsync_ShouldDoNothing_WhenIdIsGiven_AndEntityDoesNotExist()
+		{
+			// Arrange
+			var person = _entities[1];
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			await subject.RemoveAsync(person.Id).ConfigureAwait(false);
+
+			// Assert
+			subject.RawData.Count.Should().Be(0);
+		}
+
+		[Fact]
+		public async Task RemoveAsync_ShouldRemoveEntityFromRepository_WhenIdIsGiven_AndEntityExists()
+		{
+			// Arrange
+			var person = _entities[1];
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			await subject.RemoveAsync(person.Id).ConfigureAwait(false);
+
+			// Assert
+			subject.RawData.Count.Should().Be(_entities.Count - 1);
+			subject.RawData.Should().NotContainKey(person.Id);
+		}
+
+		[Fact]
+		public void TryAddAsync_ShouldThrowException_WhenEntityIsNull()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			Func<Task> action = async () => await subject.TryAddAsync(null).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public void TryAddAsync_ShouldThrowException_WhenIdIsNull()
+		{
+			// Arrange
+			var person = new FakeEntity<string>(null, "Brandon");
+			var subject = new InMemoryRepository<FakeEntity<string>, string>();
+
+			// Act
+			Func<Task> action = async () => await subject.TryAddAsync(person).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public async Task TryAddAsync_ShouldAddEntityToRepository_WhenEntityDoesNotExist()
+		{
+			// Arrange
+			var person = new FakeEntity<int>(10, "Brandon");
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			var result = await subject.TryAddAsync(person).ConfigureAwait(false);
+
+			// Assert
+			result.Should().BeTrue();
+			subject.RawData.Count.Should().Be(1);
+			subject.RawData.Should().ContainKey(person.Id);
+			subject.RawData[person.Id].Should().BeSameAs(person);
+		}
+
+		[Fact]
+		public async Task TryAddAsync_ShouldReturnFalse_WhenEntityExists()
+		{
+			// Arrange
+			var person = _entities[1];
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			var result = await subject.TryAddAsync(person).ConfigureAwait(false);
+
+			// Assert
+			result.Should().BeFalse();
+			subject.RawData.Count.Should().Be(_entities.Count);
+		}
+
+		[Fact]
+		public void TryUpdateAsync_ShouldThrowException_WhenEntityIsNull()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			Func<Task> action = async () => await subject.TryUpdateAsync(null).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public void TryUpdateAsync_ShouldThrowException_WhenIdIsNull()
+		{
+			// Arrange
+			var person = new FakeEntity<string>(null, "Brandon");
+			var subject = new InMemoryRepository<FakeEntity<string>, string>();
+
+			// Act
+			Func<Task> action = async () => await subject.TryUpdateAsync(person).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public async Task TryUpdateAsync_ShouldReturnFalse_WhenEntityDoesNotExist()
+		{
+			// Arrange
+			var person = _entities[1];
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			var result = await subject.TryUpdateAsync(person).ConfigureAwait(false);
+
+			// Assert
+			result.Should().BeFalse();
+			subject.RawData.Count.Should().Be(0);
+		}
+
+		[Fact]
+		public async Task TryUpdateAsync_ShouldUpdateEntityInRepository_WhenEntityExists()
+		{
+			// Arrange
+			var person = new FakeEntity<int>(1, "Brandon");
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			var result = await subject.TryUpdateAsync(person).ConfigureAwait(false);
+
+			// Assert
+			result.Should().BeTrue();
+			subject.RawData.Count.Should().Be(_entities.Count);
+			subject.RawData.Should().ContainKey(person.Id);
+			subject.RawData[person.Id].Should().BeSameAs(person);
+		}
+
+		[Fact]
+		public void TryRemoveAsync_ShouldThrowException_WhenEntityIsNull()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			Func<Task> action = async () => await subject.TryRemoveAsync(null).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public void TryRemoveAsync_ShouldThrowException_WhenIdIsNull()
+		{
+			// Arrange
+			var person = new FakeEntity<string>(null, "Brandon");
+			var subject = new InMemoryRepository<FakeEntity<string>, string>();
+
+			// Act
+			Func<Task> action = async () => await subject.TryRemoveAsync(person).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public async Task TryRemoveAsync_ShouldDoNothing_WhenEntityDoesNotExist()
+		{
+			// Arrange
+			var person = _entities[1];
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			var result = await subject.TryRemoveAsync(person).ConfigureAwait(false);
+
+			// Assert
+			result.Should().BeFalse();
+			subject.RawData.Count.Should().Be(0);
+		}
+
+		[Fact]
+		public async Task TryRemoveAsync_ShouldRemoveEntityFromRepository_WhenEntityExists()
+		{
+			// Arrange
+			var person = _entities[1];
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			var result = await subject.TryRemoveAsync(person).ConfigureAwait(false);
+
+			// Assert
+			result.Should().BeTrue();
+			subject.RawData.Count.Should().Be(_entities.Count - 1);
+			subject.RawData.Should().NotContainKey(person.Id);
+		}
+
+		[Fact]
+		public void TryRemoveAsync_ShouldThrowException_WhenIdIsGiven_AndIdIsNull()
+		{
+			// Arrange
+			var subject = new InMemoryRepository<FakeEntity<string>, string>();
+
+			// Act
+			Func<Task> action = async () => await subject.TryRemoveAsync((string)null).ConfigureAwait(false);
+
+			// Assert
+			action.ShouldThrow<ArgumentNullException>();
+		}
+
+		[Fact]
+		public async Task TryRemoveAsync_ShouldDoNothing_WhenIdIsGiven_AndEntityDoesNotExist()
+		{
+			// Arrange
+			var person = _entities[1];
+			var subject = new InMemoryRepository<FakeEntity<int>, int>();
+
+			// Act
+			var result = await subject.TryRemoveAsync(person.Id).ConfigureAwait(false);
+
+			// Assert
+			result.Should().BeFalse();
+			subject.RawData.Count.Should().Be(0);
+		}
+
+		[Fact]
+		public async Task TryRemoveAsync_ShouldRemoveEntityFromRepository_WhenIdIsGiven_AndEntityExists()
+		{
+			// Arrange
+			var person = _entities[1];
+			var subject = new InMemoryRepository<FakeEntity<int>, int>(_entities);
+
+			// Act
+			var result = await subject.TryRemoveAsync(person.Id).ConfigureAwait(false);
+
+			// Assert
+			result.Should().BeTrue();
+			subject.RawData.Count.Should().Be(_entities.Count - 1);
+			subject.RawData.Should().NotContainKey(person.Id);
+		}
+	}
+}
